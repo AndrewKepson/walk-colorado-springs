14575ca7672f3c1cae7ed4575c0cfa700f5347cf
RedRocksWebDevelopment <websites@redrockswebdevelopment.com>
2024-07-04 12:38:53 -0600

    Added isProdcution variable to conditionally render scripts or other resources

--- src/lib/store.js
+++ src/lib/store.js
@@ -5,6 +5,8 @@ import {
 	getWordPressPostBySlug,
 } from "./wordPressAPI";
 
+export const isProduction = import.meta.env.PROD;
+
 export const siteUrl = import.meta.env.SITE;
 
 export const useWordPressAPI = {
