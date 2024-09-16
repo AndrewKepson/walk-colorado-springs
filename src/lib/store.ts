96ddb8571ed2b783b0a97e25247e89cacc168222
RedRocksWebDevelopment <websites@redrockswebdevelopment.com>
2024-09-16 00:42:55 -0600

    bumped deps. updated image slider

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,4 +1,3 @@
-import { type FlattenedWalk } from "./wordPressAPI.types";
 import { getCollection } from "astro:content";
 
 export const isProduction = import.meta.env.PROD;
@@ -7,6 +6,6 @@ export const siteUrl = import.meta.env.SITE;
 
 // Store Collections
 const walksCollection = await getCollection("walks");
-export const allWalks: FlattenedWalk[] = [...walksCollection?.map((walk) => walk.data)].sort(
+export const allWalks = [...walksCollection?.map((walk) => walk.data)].sort(
 	(a, b) => b.walkNumber - a.walkNumber
 );
