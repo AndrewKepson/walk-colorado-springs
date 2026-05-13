81adaa396e1d6a0ae8e8fcbc953c0a41172c84db
Andrew Kepson <andyjkepson@gmail.com>
2026-05-13 17:18:02 -0600

    fix: migrate Tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-	integrations: [tailwind()],
 	trailingSlash: "never",
 	site: "https://walkcoloradosprings.com",
 	// prefetch: true,
@@ -10,5 +9,8 @@ export default defineConfig({
 	image: {
 		domains: ["jp7lua9fr7kk.walkcoloradosprings.com"],
 		remotePatterns: [{ protocol: "https" }],
+	},
+	vite: {
+		plugins: [tailwindcss()],
 	},
 });
